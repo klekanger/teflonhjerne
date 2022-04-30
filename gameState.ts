43fa0d3263e2a3b99ed96f4efb504cbdd275934a
Kurt Lekanger <kurt@lekanger.no>
2022-04-30 12:19:22 +0200

    refactor: move tiles and gameState to separate files

---
+++ gameState.ts
@@ -0,0 +1,7 @@
+// Create a global object to store state
+// Could be useful for storing more stuff in one place if we later
+// wants to expand the game with more functionality
+export const gameState = {
+  tries: 0,
+  gameOver: false,
+};
