731b81b83b85e76d802d7e18650ff4cef2c4291a
Kurt Lekanger <kurt@lekanger.no>
2022-04-30 12:19:49 +0200

    refactor: convert to TypeScript

--- gameState.js
+++
@@ -1,7 +0,0 @@
-// Create a global object to store state
-// Could be useful for storing more stuff in one place if we later
-// wants to expand the game with more functionality
-export const gameState = {
-  tries: 0,
-  gameOver: false,
-};
