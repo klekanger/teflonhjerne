7c75c5e28944554d5c1d83e218bcbcf47e7d5a57
Kurt Lekanger <kurt@lekanger.no>
2022-06-19 10:21:27 +0200

    feat: added a couple of more game sounds

--- src/lib/gamesounds.ts
+++ src/lib/gamesounds.ts
@@ -2,4 +2,6 @@ export const GAMESOUNDS = {
   flip: './audio/flip.mp3',
   match: './audio/match.mp3',
   win: './audio/win.mp3',
+  newGame: './audio/newtiles.wav',
+  uhOh: './audio/sfx_sound_nagger2.wav',
 };
